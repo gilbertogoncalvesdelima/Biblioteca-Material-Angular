c1842edf90f477cd2de32063775fcff0317d0e85
gilbertogoncalvesdelima <gilberto-goncalves@outlook.com.br>
2024-01-03 22:11:25 -0300

    Refatoração no codigo

--- jornada-milhas/src/app/core/services/form-busca.service.ts
+++ jornada-milhas/src/app/core/services/form-busca.service.ts
@@ -6,12 +6,22 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class FormBuscaService {
 
-  // Controle de formularios, FormGroup
-  formBusca: FormGroup
+  formBusca: FormGroup;
 
-  constructor() {
-  this.formBusca = new FormGroup({
-    somenteIda :new FormControl(false)
-  })
-   }
+  constructor() { 
+
+    this.formBusca = new FormGroup({
+      somenteIda: new FormControl(false),
+      origem: new FormControl(null),
+      destino: new FormControl(null),
+    })
+  }
+
+  obterControle(nome:string): FormControl {
+    const control = this.formBusca.get(nome);
+    if (!control) {
+      throw new Error(`FormControl com nome "${nome}" não existe.`);
+    }
+    return control as FormControl;
+  }
 }
